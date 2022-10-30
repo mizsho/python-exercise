5844528bc645d57cc2e43e845a74599566e2e5c5
mizsho <34414292+mizsho@users.noreply.github.com>
2022-10-30 20:35:34 +0900

    multiprocessingでProcessを使う影響だけの確認

--- parallelization/multiprocessing-test.py
+++ parallelization/multiprocessing-test.py
@@ -32,18 +32,20 @@ def calc_dist_max(arr):
     return idx_dist_max
 
 # %%
-# 1000万点を用意
+# 100万点を用意
 pow_arr = 6
 pow_partial_arr = pow_arr - 2
 arr = np.random.random((10**pow_arr,2))
 
 # %%
 # 並列化無しで、100点ごとに距離の最大値を計算
+# 実行時間：2.5s
 for i in range(10**pow_partial_arr):
     _ = calc_dist_max(arr[100*i:100*(i+1)])
 
 # %%
 # 並列化ありで、100点ごとに距離の最大値を計算
+# 実行時間：24.8s（ちょうど10倍くらい）
 for i in range(10**(pow_partial_arr-1)):
     list_process = []
     for j in range(10):
@@ -56,6 +58,14 @@ for i in range(10**(pow_partial_arr-1)):
 
     for p in list_process:
         p.join()
+
+# %%
+# processのオーバーヘッドを確認
+# 実行時間：42.7s
+for i in range(10**pow_partial_arr):
+    myprocess = Process(target=calc_dist_max, args=(arr[100*i:100*(i+1)],))
+    myprocess.start()
+    myprocess.join()
 
 # %%
 arr = None
